3299f48b4e25394d431d2a674fc3d4d6985f92e0
peterdresslar <peterdresslar@gmail.com>
2023-09-22 13:48:01 -1000

    bad import fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useChat } from 'ai/react';
-import { use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Chat() {
   const [providerNickname, setProviderNickname] = useState('Claude');
