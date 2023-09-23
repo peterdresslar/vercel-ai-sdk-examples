3fdbfd24b79d86562c06f64085558a66e4e676b3
peterdresslar <peterdresslar@gmail.com>
2023-09-22 15:03:31 -1000

    Chat updated, both examples working, cool cool cool

--- app/chat.tsx
+++ app/chat.tsx
@@ -9,7 +9,7 @@ export default function Chat() {
   const [enableLog, setEnableLog] = useState(false);
   const { messages, input, handleInputChange, handleSubmit, isLoading, error } =
     useChat({
-      api: './api/chat/anthropic-sdk',
+      api: './api/chat/anthropic',
       // add an onFinish callback to set the success flag
       onFinish: () => {
         setSuccessFlag(true);
