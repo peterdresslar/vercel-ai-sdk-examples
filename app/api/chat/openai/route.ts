39b62dd03a17a296703e856b097b638a9c1bef77
peterdresslar <peterdresslar@gmail.com>
2023-09-28 11:19:05 -1000

    Supabase saves

--- app/api/chat/openai/route.ts
+++ app/api/chat/openai/route.ts
@@ -1,10 +1,32 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '');
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+// Save messages and return the record id.
+const saveToSupabase = async (messages: String, result: String) => {
+  let { data, error } = await supabase
+    .from('transactions')
+    .insert([{ messages: messages, response: result }])
+    .select();
+  if (error) {
+    console.log('m ' + error.message);
+    console.log('h ' + error.hint);
+    console.log('d ' + error.details);
+    return -1;
+  }
+  if (data) {
+    return data[0].id;
+  }
+};
 
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
@@ -20,7 +42,15 @@ export async function POST(req: Request) {
     messages,
   });
   // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
+  const stream = OpenAIStream(response, {
+    onFinal: async (result: string) => {
+      // Save messages, response to supabase and return the record so we can send the id and created_at to the client
+      console.log(result);
+      const id = await saveToSupabase(JSON.stringify(messages), result);
+      console.log(id);
+    },
+  });
+
   // Respond with the stream
   return new StreamingTextResponse(stream);
 }
